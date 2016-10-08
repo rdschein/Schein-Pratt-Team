d2330943e9b51c6ddf942fd6482b786f1266131a
Dylan <Dylan@10.38.167.11>
2016-10-07 18:27:43 -0600

    Player and Location class are now functional

--- dungeonHero/src/dungeonhero/Location.java
+++ dungeonHero/src/dungeonhero/Location.java
@@ -5,10 +5,83 @@
  */
 package dungeonhero;
 
+
+import java.io.Serializable;
+import java.util.Objects;
 /**
  *
  * @author Dylan
  */
-public class Location {
+public class Location implements Serializable {
+    
+    private int row;
+    private int column;
+    private boolean visited;
+
+    public Location() {
+    }
+    
+    
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + this.row;
+        hash = 97 * hash + this.column;
+        hash = 97 * hash + (this.visited ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "row=" + row + ", column=" + column + ", visited=" + visited + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.column != other.column) {
+            return false;
+        }
+        if (this.visited != other.visited) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
