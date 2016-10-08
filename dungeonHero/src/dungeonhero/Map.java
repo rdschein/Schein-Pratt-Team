b1b86b8d206b2efa3d4edfea6bc3d3658ae9a80e
Dylan <Dylan@10.38.167.11>
2016-10-07 19:22:32 -0600

    Player and Location class are now functional and the map

--- dungeonHero/src/dungeonhero/Map.java
+++ dungeonHero/src/dungeonhero/Map.java
@@ -5,10 +5,73 @@
  */
 package dungeonhero;
 
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
 /**
  *
  * @author Dylan
  */
 public class Map {
+    
+    private int numOfRows;
+    private int numOfCol;
+
+    public Map() {
+    }
+    
+    
+
+    public int getNumOfRows() {
+        return numOfRows;
+    }
+
+    public void setNumOfRows(int numOfRows) {
+        this.numOfRows = numOfRows;
+    }
+
+    public int getNumOfCol() {
+        return numOfCol;
+    }
+
+    public void setNumOfCol(int numOfCol) {
+        this.numOfCol = numOfCol;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + this.numOfRows;
+        hash = 47 * hash + this.numOfCol;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "numOfRows=" + numOfRows + ", numOfCol=" + numOfCol + '}';
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.numOfRows != other.numOfRows) {
+            return false;
+        }
+        if (this.numOfCol != other.numOfCol) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
