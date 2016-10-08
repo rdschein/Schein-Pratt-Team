9dfbde66d4eb9d209da8ed222662b0dec1b50af7
Dylan <Dylan@10.38.167.11>
2016-10-07 18:03:55 -0600

    I have made the first class. It was successfull go team!

---
+++ dungeonHero/src/dungeonhero/Player.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeonhero;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author Dylan
+ */
+public class Player implements Serializable {
+    
+    
+    private String name;
+    private int gameOverCount;
+
+    public Player() {
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGameOverCount() {
+        return gameOverCount;
+    }
+
+    public void setGameOverCount(int gameOverCount) {
+        this.gameOverCount = gameOverCount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.name);
+        hash = 61 * hash + this.gameOverCount;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", gameOverCount=" + gameOverCount + '}';
+    }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.gameOverCount != other.gameOverCount) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
+
+
+
+
