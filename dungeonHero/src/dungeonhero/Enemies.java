761ecb8f510351471be5d827bcc645eacaa442f3
Rob <Rob@10.38.165.112>
2016-10-08 21:40:48 -0600

    Player Class Implemented

--- dungeonHero/src/dungeonhero/Enemies.java
+++ dungeonHero/src/dungeonhero/Enemies.java
@@ -5,10 +5,75 @@
  */
 package dungeonhero;
 
+import java.io.Serializable;
+import java.util.Objects;
 /**
  *
  * @author Dylan
  */
-public class Enemies {
+public class Enemies implements Serializable {
+    
+    private String type;
+    private String attributes;
+
+    public Enemies() {
+    }
+    
+    
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(String attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.type);
+        hash = 97 * hash + Objects.hashCode(this.attributes);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Enemies{" + "type=" + type + ", attributes=" + attributes + '}';
+    }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Enemies other = (Enemies) obj;
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.attributes, other.attributes)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     
 }
