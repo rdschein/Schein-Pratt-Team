52aa7f8e8abe8db1061163733dd8b5272a430997
Dylan <Dylan@DylanJohn>
2016-10-26 09:58:39 -0600

    Rob Dylan SUper team

---
+++ dungeonHero/src/dungeonhero/view/StartProgramView.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeonhero.view;
+
+/**
+ *
+ * @author Dylan
+ */
+public class StartProgramView {
+
+    
+    
+    private String promptMessage;
+    
+    
+    public StartProgramView()
+    {
+        this.promptMessage = "Please enter your Name:";
+       //banner 
+        this.displayBanner();
+       
+    }
+    public void displayStartProgramView()
+    {
+        // Print the Banner page with short discription
+        
+       //Prompt for Player name
+        
+        // Call control function to create a player
+        
+        //If the player is created, display a welcome message and main mnu: else display error and repeat
+        
+    }
+
+    private void displayBanner() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
