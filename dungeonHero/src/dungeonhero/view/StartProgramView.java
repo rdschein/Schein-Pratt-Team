f8929d157d7248dae7617cfb2fbbd015e92f28c7
Dylan <Dylan@DylanJohn>
2016-10-28 09:51:55 -0600

    Rob Dylan SUper team

--- dungeonHero/src/dungeonhero/view/StartProgramView.java
+++ dungeonHero/src/dungeonhero/view/StartProgramView.java
@@ -5,6 +5,10 @@
  */
 package dungeonhero.view;
 
+import dungeonhero.Player;
+import dungeonhero.control.GameControl;
+import java.util.Scanner;
+
 /**
  *
  * @author Dylan
@@ -28,6 +32,16 @@ public class StartProgramView {
         // Print the Banner page with short discription
         
        //Prompt for Player name
+        boolean done = false; // set flag to not done
+        do{
+            // prompt for and get players name
+            String playersName = this.getPlayerName();
+            if (playersName.toUpperCase().equals("Q")) // user wants to quit
+                return; // exit game
+            
+            // do the requested action and display the next view
+            done = this.doAction(playersName);
+        } while (!done);
         
         // Call control function to create a player
         
@@ -35,8 +49,64 @@ public class StartProgramView {
         
     }
 
-    private void displayBanner() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void displayBanner() 
+    {
+     System.out.println(
+     "\n**********************************************************************"
+   + "\n*                                                                    *"
+   + "\n*Dungeon Heroes is a text based action role palying game. You will   *"
+   + "\n*need to explore the dungeon and find hidden items to reveal the     *"
+   + "\n*secrets of the Dungeon. Have Courage and Conquer the Dungeon!       *"
+   + "\n*                                                                    *"
+   + "\n**********************************************************************"
+             
+     
+     
+     );    //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private String getPlayerName() {
+       Scanner keyboard = new Scanner(System.in); // get from keyboard
+       String value ="";// returned name
+       boolean valid = false;
+       
+       while(!valid){
+           System.out.println("\n"+this.promptMessage);
+           
+           value = keyboard.nextLine();
+           value = value.trim();
+           
+           if(value.length()<1){
+               System.out.println("\nInvalid value: value can not be blank");
+               continue;
+           }
+       
+           break; // end loop
+       }
+       return value;
+    }
+
+    private boolean doAction(String playersName) {
+        if (playersName.length() < 2) {
+            System.out.println("\nInvalid players name: "
+                   + "The name must be greater than one character in length");
+            return false;
+        }
+        
+        Player player = GameControl.createPlayer(playersName);
+        
+        if (player == null) {
+            System.out.println("\nError creating the player.");
+            return false;
+        }
+        
+        this.displayNextView(player);
+        
+        return true;
+    }
+
+    private void displayNextView(Player player) {
+        System.out.println("\n*** displayNextView() called ***");
     }
 
     
