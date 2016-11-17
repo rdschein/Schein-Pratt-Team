4a0672de8f895a1fb809eadf0ede43df5ef9e63c
Dylan Pratt <Dylan Pratt@DESKTOP-K4STN5O>
2016-11-17 14:50:18 -0700

    Dylan assignment

--- dungeonHero/src/dungeonhero/Actor.java
+++ dungeonHero/src/dungeonhero/Actor.java
@@ -3,36 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dungeonhero;
 
-import java.io.Serializable;
-import java.awt.Point;
 
-/**
- *
- * @author Rob
- */
-public enum Actor implements Serializable {
-    
-    Goblin("Evil underling of the dungeon.", 1, 1),
-    Ghost("Evil spirit that haunts the dungeon.", 1, 4),
-    Knight("Noble soldier who guards the dungeon.", 2, 2),
-    Undead("Skeleton that haunts the dungeon.", 4, 3);
-    
-    private final String description;
-    private final Point coordinates;
-    
-    Actor(String description, int row, int column) {
-        this.description = description;
-        this.coordinates = new Point(row, column);
-    }
-    
-    public String getDescription() {
-        return description;
-    }
-    
-    public Point getCoordinates() {
-        return coordinates;
-    }
 
-}
