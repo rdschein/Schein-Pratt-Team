ff0d2c885ef5d19501ab87c499bc02627cf619e4
Rob <Rob@10.38.165.112>
2016-12-13 15:57:17 -0700

    Merge origin/master
    Conflicts:
    	dungeonHero/src/dungeonhero/Scene.java

--- dungeonHero/src/dungeonhero/view/HelpMenuView.java
+++ dungeonHero/src/dungeonhero/view/HelpMenuView.java
@@ -25,9 +25,10 @@ public class HelpMenuView extends View {
                   + "\n  Help Menu                      "
                   + "\n---------------------------------"
                   + "\nH-How to Play                    "
-                  + "\nM-How to Move?                   "
-                  + "\nL-What to look for?              "
-                  + "\nI-Finding items                  "
+                  + "\nM-How to Move                    "
+                  + "\nA-How to Attack                  "
+                  + "\nL-Acquiring and Using Items      "
+                  + "\nI-List of Items                  "
                   + "\nQ-Exit help menu                 "
                   + "\n---------------------------------");
  }
@@ -39,13 +40,60 @@ public class HelpMenuView extends View {
         
         switch(choice){
             case "H": // creat new game
-                System.out.println("How to play instructions her");
+                System.out.println("Dungeon Hero is a game where the player moves\n"
+                        + "around and explores a dungeon by entering commands that\n"
+                        + "will be explained in other areas of the help menu.\n"
+                        + "The hero (you!) will adventure through the dungeon and\n"
+                        + "defeat monsters and overcome obstacles along the way.\n"
+                        + "You will have a supply of items that can be used to\n"
+                        + "help you in various scenarios in the dungeon. For example,\n"
+                        + "you may use the enchanted sword to defeat powerful enemies.\n"
+                        + "Use your items, defeat enemies, explore the dungeon, and defeat\n"
+                        + "the dungeon boss to become the Dungeon Hero.");
                 break;
             case "M":
-                System.out.println("How to move instructions her");
+                System.out.println("Movement is essential in Dungeon Hero since you\n"
+                        + "must move to new rooms to encounter new enemies and progress\n"
+                        + "through the dungeon. By entering commands to move north, south,\n"
+                        + "east, and west, the player can move one position at a time\n"
+                        + "in a given direction. The console will inform the player of\n"
+                        + "whether or not they are able to travel the direction entered.\n"
+                        + "Sometimes, obstacles and walls will block your path so\n"
+                        + "you must enter a new path command. \n"
+                        + "The commands for movement are as follows:\n\n"
+                        + "Move North: go north\n" 
+                        + "Move South: go south\n" 
+                        + "Move East: go east\n" 
+                        + "Move West: go west");
                 break;
             case "L":
-                System.out.println("How to play instructions her");
+                System.out.println("Items have various usages in Dungeon Hero.\n"
+                        + " You have a supply of items and weapons that will be\n"
+                        + "used throughout the dungeon to win the game. Items to\n"
+                        + "help you will need to be acquired. The bombs can be\n"
+                        + "used to destroy crumbling walls and the grappling hook\n"
+                        + "can be used to swing over pits of fire. There are command\n"
+                        + "to use these items to eliminate the obstacle at hand.\n"
+                        + "These commands are as follows:\n\n"
+                        + "Pick Up Bow: grab bow\n" 
+                        + "Pick Up Harp: grab harp\n" 
+                        + "Pick Up Enchanted Sword: grab sword\n" 
+                        + "Pick Up Bombs: grab bombs\n" 
+                        + "Pick Up Grappling Hook: grab grappling hook\n" 
+                        + "Use Bombs: use bombs on walls\n" 
+                        + "Use Grappling Hook: use grappling hook");
+                break;
+            case "A":
+                System.out.println("Attacking is important to the game as well.\n"
+                        + " You can’t beat the game without fighting off the\n"
+                        + " monsters after all! When you enter a new room,\n"
+                        + " a description pops up explaining to you what is in the room.\n"
+                        + " If a monster appears you have the option to attack.\n"
+                        + " Not all monsters are weak to the same weapon!!! A floating\n"
+                        + " monster such as a ghost may not be able to be defeats with a sword!\n"
+                        + " To attack you can use the following commands:\n\n"
+                        + "Attack With Sword: use sword\n"
+                        + "Attack With Bow: use bow");
                 break;
             case "I":
                 System.out.println("Possible items in game");
